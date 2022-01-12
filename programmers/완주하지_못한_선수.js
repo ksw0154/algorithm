06a6381cd76498f6bb09b7540b057d93675a1056
ksw0154 <rlatkddnjs44@naver.com>
2022-01-12 17:33:10 +0900

    Lv.1 완주하지 못한 선수

---
+++ programmers/완주하지_못한_선수.js
@@ -0,0 +1,21 @@
+const participant = ["mislav", "stanko", "mislav", "ana"];
+const completion = ["stanko", "ana", "mislav"];
+
+function solution(participant, completion) {
+  let answer = "";
+  participant.sort();
+  completion.sort();
+
+  for (let i = 0; i < participant.length; i++) {
+    if (participant[i] != completion[i]) {
+      answer = participant[i];
+      break;
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution(participant, completion));
+
+function solution(participant, completion) {}
