1ac4bd29501274805a109456c75364b4ca95ce54
ksw0154 <rlatkddnjs44@naver.com>
2022-01-19 15:51:49 +0900

    Lv.1 콜라츠 추측

---
+++ programmers/콜라츠_추측.js
@@ -0,0 +1,16 @@
+const num = 626331;
+
+function solution(num) {
+  let count = 0;
+  while (num != 1) {
+    num = num % 2 == 0 ? num / 2 : num * 3 + 1;
+    count++;
+    if (count >= 500) {
+      return -1;
+    }
+  }
+
+  return count;
+}
+
+console.log(solution(num));
