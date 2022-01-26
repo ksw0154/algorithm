f77d7b02da322bd3cb473b41390f8742ef7a905d
ksw0154 <rlatkddnjs44@naver.com>
2022-01-26 22:18:09 +0900

    Lv.1 문자열 내 p와 y의 개수

---
+++ programmers/문자열_내_p와_y의_개수.js
@@ -0,0 +1,18 @@
+const s = "pPoooyY";
+
+function solution(s) {
+  s = s.toLowerCase();
+  let pCount = 0;
+  let yCount = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "p") {
+      pCount++;
+    } else if (s[i] === "y") {
+      yCount++;
+    }
+  }
+
+  return pCount === yCount ? true : false;
+}
+
+console.log(solution(s));
