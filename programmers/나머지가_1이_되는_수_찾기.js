d9541585c5299137c9720ddceae8ceb364775226
ksw0154 <rlatkddnjs44@naver.com>
2022-01-29 21:11:55 +0900

    Lv.1 나머지가 1이 되는 수 찾기

---
+++ programmers/나머지가_1이_되는_수_찾기.js
@@ -0,0 +1,15 @@
+const n = 12;
+
+function solution(n) {
+  var answer = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (n % i == 1) {
+      answer = i;
+      break;
+    }
+  }
+  return answer;
+}
+
+console.log(solution(n));
